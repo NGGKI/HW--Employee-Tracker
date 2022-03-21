3a72db3780d16590922c00c6a27eb9983882019e
NGGKI <kien.dv2012@gmail.com>
2022-03-21 15:25:41 -0700

    update sql

--- db/index.js
+++ db/index.js
@@ -1,1 +1,1 @@
-module.exports = require('mysql2').createConnection('mysql://root:Hnkie2796$@localhost:3306/favorites_db')
+module.exports = require('mysql2').createConnection('mysql://root:Hnkie2796$@localhost:3306/employees')
